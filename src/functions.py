ece179798943a400ba0f05d6a3dead1304292dca
bravodude <85579815+bravodude@users.noreply.github.com>
2024-05-04 01:58:03 -0500

    add more function definitions

--- src/functions.py
+++ src/functions.py
@@ -1,2 +1,19 @@
+# FUNCTIONS THAT TAKE INPUT
+def get_time(): # get month day year hour minutes
+    print("test")
+
+def focus_rating():
+    print("test")
+
+def levels():
+    print("test")
+
+def food():
+    print("test")
+
+
+
+
+# FUNCTIONS THAT OUTPUT
 def send_data (): # tasked with sending data to dataset.csv
-    
+    print("test")

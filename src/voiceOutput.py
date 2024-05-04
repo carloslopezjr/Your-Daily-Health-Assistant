20ae11ddc1c1990a13a09ae718760825f38f7c21
bravodude <85579815+bravodude@users.noreply.github.com>
2024-05-04 00:33:43 -0500

    first code

---
+++ src/voiceOutput.py
@@ -0,0 +1,34 @@
+# once the sensor is registered, we want to check the time of day
+from datetime import datetime
+
+# if time is in between 7am - 11am and it's the first time they passed through, then we want to greet them
+time = datetime.now()
+times_passed = 0 # this is the amount of times the sensor has registered
+
+if times_passed == 0:
+    print("Good morning Sir, I hope you had good sleep, it is time to do your daily health check.")
+    times_passed += 1
+
+    # ask how they feel from 1 - 10
+    # ask them to check their moring gluclose level
+    # give them the stats for the previous day and week
+
+else:
+    print("How's it going sir, did you want to do a quick health check?")
+    times_passed += 1
+
+    # how do you currently feel fomr 1 - 10
+    # what did you eat anything for breakfast?
+        # what did you eat?
+        # at what time
+        # what's your current blood sugar level
+    
+    # did you eat anything else?
+        # repeat questions
+    
+    # I see that it's lunch/dinner time, did you plan on eating again?
+
+    # alright that's all I needed, talk soon
+
+# then have times_passed reset whenever 3 am hits
+

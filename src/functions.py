e4382e56a582d5840d4bf7b099da64fbca84d131
bravodude <85579815+bravodude@users.noreply.github.com>
2024-05-04 13:46:24 -0500

    delete in future

--- src/functions.py
+++ src/functions.py
@@ -1,19 +1,33 @@
 # FUNCTIONS THAT TAKE INPUT
-def get_time(): # get month day year hour minutes
-    print("test")
+def get_time(time, array) : # get month day year hour minutes and append into array
 
-def focus_rating():
-    print("test")
+    array[0] = time.year
+    array[1] = time.month
+    array[2] = time.day
+    array[3] = time.hour
+    array[4] = time.minute
 
-def levels():
-    print("test")
 
-def food():
-    print("test")
+def levels(array): # get level input from user
+    level = input("Input your levels: ")
+    array[5] = int(level)
+
+
+
+def focus_rating(array): # get focus rating input from user
+    focus = input("Input your focus rate: ")
+    array[6] = int(focus)
+
+
+def food(array): # get food input from user
+    food = input("Input the food you ate: ")
+    array[7] = food
 
 
 
 
 # FUNCTIONS THAT OUTPUT
-def send_data (): # tasked with sending data to dataset.csv
-    print("test")
+def send_data (filename, arr): # tasked with sending data to dataset.csv
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows()

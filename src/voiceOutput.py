63a49e0fec0a491a01741e9326a9a9812d8e8e40
bravodude <85579815+bravodude@users.noreply.github.com>
2024-05-04 13:46:47 -0500

    add functions in file

--- src/voiceOutput.py
+++ src/voiceOutput.py
@@ -1,21 +1,66 @@
-from functions import send_data
-
-# once the sensor is registered, we want to check the time of day
+from functions import *
+import csv
 from datetime import datetime
+
+
+# FUNCTIONS THAT TAKE INPUT
+def get_time(time, array) : # get month day year hour minutes and append into array
+
+    array[0] = time.year
+    array[1] = time.month
+    array[2] = time.day
+    array[3] = time.hour
+    array[4] = time.minute
+
+def levels(array): # get level input from user
+    level = input("Input your levels: ")
+    array[5] = int(level)
+
+def focus_rating(array): # get focus rating input from user
+    focus = input("Input your focus rate: ")
+    array[6] = int(focus)
+
+def food(array): # get food input from user
+    food = input("Input the food you ate: ")
+    array[7] = food
+
+# FUNCTIONS THAT OUTPUT
+def send_data (filename, arr): # tasked with sending data to dataset.csv
+
+    
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(arr)
+
+
 
 # if time is in between 7am - 11am and it's the first time they passed through, then we want to greet them
 time = datetime.now()
+
 times_passed = 0 # this is the amount of times the sensor has registered
 
 if times_passed == 0:
     print("Good morning Sir, I hope you had good sleep, it is time to do your daily health check.")
     times_passed += 1
 
-    send_data()
+    filename = '/Users/carlos/Desktop/Your-Daily-Health-Assistant/data/dataset.csv'
 
-    # ask how they feel from 1 - 10
-    # ask them to check their moring gluclose level
+    # hold values before pushed to the csv
+    arr = [0, 0, 0, 0, 0, 0, 0, 0]
+
+    get_time(time, arr)
+    levels(arr) # ask them to check their moring gluclose level
+    focus_rating(arr) # ask how they feel from 1 - 10
+    food(arr) # ask for their food
+
+
+    send_data(filename, arr)
+
+    # print(arr) # test
+
     # give them the stats for the previous day and week
+
+
 
 else:
     print("How's it going sir, did you want to do a quick health check?")

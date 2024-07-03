5311bc19526b6f4df36a8a955867bbb228cb2163
carloslopezjr <carlosssolrac1@gmail.com>
2024-07-02 23:19:10 -0500

    addition of code to run bash script

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <wiringPi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define PIR_PIN 26  // WiringPi pin 0 is BCM_GPIO 17
 
@@ -14,13 +15,26 @@ int main(void) {
 
     printf("PIR Sensor Test (CTRL+C to exit)\n");
 
+    const char *python_file = "functions.py";
+
     while (1) {
         if (digitalRead(PIR_PIN) == HIGH) {
-            printf("Motion Detected!\n");
-            // delay(1000);  // Wait for 1 second
+            printf("Motion Detected! Starting Program\n");
+            char command[256];
+            snprintf(command, sizeof(command), "./run_python.sh %s", python_file);
+
+            int result = system(command);
+
+            if (result == -1) {
+                perror("system");
+                return 1;
+            }
+
+            break;
+            // delay(10000);  // Wait for 10 second
         } else {
             printf("No Motion\n");
-            // delay(1000);  // Wait for 1 second
+            delay(1000);  // Wait for 1 second
         }
     }
 

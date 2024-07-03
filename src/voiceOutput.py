bd63e9595db8c69950b67db5c2e5c23daacff711
carloslopezjr <carlosssolrac1@gmail.com>
2024-07-02 23:20:04 -0500

    cleaning up old functions

--- src/voiceOutput.py
+++ src/voiceOutput.py
@@ -3,13 +3,19 @@ from contextlib import redirect_stderr
 import io
 
 
+def microphone_names():
+    # f = io.StringIO()
+    # with redirect_stderr(f):
+    #     r = sr.Recognizer()
+    #     mic_list = sr.Microphone.list_microphone_names()
 
-# f = io.StringIO()
-# with redirect_stderr(f):
-#     r = sr.Recognizer()
-#     mic_list = sr.Microphone.list_microphone_names()
+    # print("Available microphones:")
+    # for index, name in enumerate(mic_list):
+    #     print(f"{index}: {name}") 
 
 
+
+# Code that works with right microphone, use microphone_names() to find what device_index to use
 r = sr.Recognizer()
 r.energy_threshold = 500
 r.pause_threshold = 0.5
@@ -17,7 +23,6 @@ with sr.Microphone(device_index = 1) as source:
     print("Say something!")
     audio = r.listen(source)
     
-
 
 try:
     print("We think you said: " + r.recognize_google(audio))
@@ -27,9 +32,3 @@ except sr.RequestError:
     print("Could not request results from Google")
 
 
-
-
-# # main() # start the process
-# print("Available microphones:")
-# for index, name in enumerate(mic_list):
-#     print(f"{index}: {name}") 
